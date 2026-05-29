6540d2ddaac5094988151e4d0fafdeeadadcc6be
Razilkaa <kiamov123@gmail.com>
2026-05-29 20:10:41 +0300

    v0.2 faceit player info hardcode

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,29 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-app = FastAPI();
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+class PlayerProfile(BaseModel):
+    nickname: str
+    country: str
+    level: int
+    elo: int
+    matches: int
 
 @app.get("/api/health")
 def health_check():
     return {"status": "ok"}
+
+
+@app.get("/api/players/{nickname}", response_model=PlayerProfile)
+def get_player(nickname:str):
+    return PlayerProfile(
+        nickname=nickname,
+        country="Russia",
+        level=10,
+        elo=2001,
+        matches=724,
+)
 app.mount("/", StaticFiles(directory="../frontend", html=True), name="static")
--- frontend/main.js
+++ frontend/main.js
@@ -1,14 +1,38 @@
-const elHealth = document.getElementById('health');
+const elhealth = document.getElementById("health");
+const elinput = document.getElementById("nickInput");
+
+const elsearchBtn = document.getElementById("searchBtn");
+const elprofile = document.getElementById("profile");
+elsearchBtn.addEventListener("click", () => {
+    const nickname = elinput.value;
+    if (!nickname) {
+        elprofile.textContent = "Введите никнейм"
+        return;
+    }
+    const url = `/api/players/${nickname}`;
+    fetch(url)
+    .then(response => response.json())
+    .then(data =>{
+        elprofile.innerHTML = `
+        <h2>${data.nickname}</h2>
+        <p>Country: ${data.country}</p>
+        <p>Level: ${data.level}</p>
+        <p>ELO: ${data.elo}</p>
+        `
+    } )
+    .catch(() => {
+        elprofile.innerHTML = "<p>Игрок не найден</p>"  
+    });
+});
 function checkBackend() {
-    elHealth.textContent = 'checking...';
+    elhealth.textContent = 'checking...';
     fetch('/api/health')
     .then(response => response.json())
-    .then(data => {elHealth.textContent = data.status === "ok" ? "🟢 online" : "🔴 offline"})
+    .then(data => {elhealth.textContent = data.status === "ok" ? "🟢 online" : "🔴 offline"})
     .catch(() => {
-        elHealth.textContent = "🔴 offline"
+        elhealth.textContent = "🔴 offline"
     })
 
 }
 
 checkBackend();
-
